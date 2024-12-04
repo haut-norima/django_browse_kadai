c2df18a1f066b0cbc421a0eae89c530ff230c942
Kazuyuki Norimatsu <norimatsu@atmospl.com>
2024-12-05 00:20:58 +0900

    db接続設定

--- docroot/settings.py
+++ docroot/settings.py
@@ -1,1 +1,14 @@
+import os
+
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'default_db_name'),
+        'USER': os.getenv('DB_USER', 'default_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'default_password'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # 'localhost' はローカル開発時のデフォルト
+        'PORT': os.getenv('DB_PORT', '5432'),        # PostgreSQL のデフォルトポート
+    }
+}
